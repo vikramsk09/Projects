594cffea1f18dfc600a1e154ceacf3591ca9a605
Vikram Singh Kushwaha <vikramsinghkushwaha09@gmail.com>
2025-12-01 18:38:27 +0530

    Add files via upload
    In this project, a user can first Register by entering a 'Username' and 'Password', which the program stores.
    After registration, the user can Login using the same credentials.
    
    Here’s how it works:
    ✔️ If the entered username and password match — Login Successful
    ❌ If either is incorrect — Login Failed and the user is asked to try again
    
    While building this system, several concepts used:
    🔹 Working with input functions
    🔹 Conditional statements
    🔹 Looping
    🔹 Basic validation & logic structure
    🔹 Storing and verifying user data

---
+++ Login_and_Signup_system.py
@@ -0,0 +1,134 @@
+# Project : Building a Login and Signup system
+
+print("***Create an account***\n")
+
+username = input("Username: ")
+password = input("Password: ")
+
+print("***Registered successfully!***")
+print("\nLogin to your account")
+
+
+while True:
+    username2 = input("Username: ")
+    password2 = input("Password: ")
+    if username == username2 and password == password2:
+        print("Login successful!")
+        break
+    else:
+        print("\nInvalid credentials.\nPlease check you 'Username' or 'Password'\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
